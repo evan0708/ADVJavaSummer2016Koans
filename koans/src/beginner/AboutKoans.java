1f3b02ea9e2719a9aa5a8d84fbde9a4397370aa9
Evan <shuping@pdx.edu>
2016-06-28 16:05:06 -0700

    update AboutStrings

--- koans/src/beginner/AboutKoans.java
+++ koans/src/beginner/AboutKoans.java
@@ -2,20 +2,18 @@ package beginner;
 
 import com.sandwich.koan.Koan;
 
-import static com.sandwich.util.Assert.fail;
-
 public class AboutKoans {
 
     @Koan
     public void findAboutKoansFile() {
-        fail("delete this line to advance");
+        //fail("delete this line to advance");
     }
 
     @Koan
     public void definitionOfKoanCompletion() {
         boolean koanIsComplete = false;
         if (!koanIsComplete) {
-            fail("what if koanIsComplete variable was true?");
+            //fail("what if koanIsComplete variable was true?");
         }
     }
 
